2dc65487ff169914f85f4c2984e4637ff5c0ca07
Daniel <marmandu@gmail.com>
2023-03-01 15:42:18 +0000

    change url name

--- update/urls.py
+++ update/urls.py
@@ -3,7 +3,7 @@ from update import views
 
 urlpatterns = [
     #path('', views.update),
-    path('update/', views.selectStudy, name='selectStudy'),
-    path('update/<study_id>', views.updatePlot, name='updatePlot'),
+    path('display/', views.selectStudy, name='selectStudy'),
+    path('display/<study_id>', views.updatePlot, name='updatePlot'),
     path('plotDetails/<study_id>/<plot_id>', views.plotDetails, name='plotDetails'),
 ]
